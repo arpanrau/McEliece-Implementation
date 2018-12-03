2454ddcd42b16fe3b4eafe7c1f9c074abbc044f8
arpanrau <arpan.rau@gmail.com>
2018-12-03 10:33:35 -0500

    Finished keygen

--- keygen.py
+++ keygen.py
@@ -1,26 +1,35 @@
 import numpy as np
-import scipy as sp
+
 
 class hamming_keygen:
-    def __init__(self, k,n, S, P, m):
-        self.k = k
-        self.wN = -2**(-k-1)*np.log10(2)
-        self.kgen = -1*((scipy.lambertw(self.wN)+k*np.log10(2)+log10(2))/np.log10(2)
-        self.n = genN(self)
+    def __init__(self, k,n,kgen,S,P,m):
+        self.kgen = kgen
+        self.k = 2**self.kgen - self.kgen -1
+        self.n = 2**self.kgen -1
         self.G = self.genhammingMatrix()
         self.S = self.genInvertibleMatrix()
         self.P = self.genPermuteMatrix()
-        self.Gcarat = SGP
+        self.Gcarat = np.matmul(np.matmul(self.S,self.G),self.P)
         self.m = m
-
-    def genN(self):
-        self.n = 2**self.kgen - 1
 
     def genHammingMatrix(self):
         """Generates Hamming Matrix given k and n"""
-        self.klength = self.k - self.n
-        self.identity = numpy.identity(self.klength)
-        
+        self.identity = numpy.identity(self.kgen)
+        self.identityk = numpy.identity(self.k)
+        self.left = np.zeroes(self.kgen,2**self.kgen - 1 - self.kgen)
+        self.rowcount = 0
+        for i in range(2**self.kgen):
+            if i+1 != 1:
+                if (1+1)%2 != 0:
+                    self.binarystring = np.binary_repr(i+1)
+                    self.column = np.zeros((length(self.binarystring),1))
+                    for i in binarystring:
+                        self.column[0][i] = int(i)
+                    self.column = np.pad(self.column, (1,self.kgen), 'constant')
+                    self.left[:][count] =  self.column
+        self.paritycheck = np.concatenate(self.left,self.identity)
+        self.generator = np.concatenate(self.identityk,np.transpose(self.left))
+        return self.generator
 
     def genInvertibleMatrix(self):
         self.S = np.random.randint(0,2,(self.k,self.k), dtype=np.uint)
