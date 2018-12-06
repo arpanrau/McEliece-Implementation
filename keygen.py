ead306d2ea990d9e6849fbbbf92140079504da19
nrivkin <noah.rivkin@students.olin.edu>
2018-12-06 10:20:13 -0500

    Fix errors in keygen.py

--- keygen.py
+++ keygen.py
@@ -2,42 +2,46 @@ import numpy as np
 
 
 class hamming_keygen:
-    def __init__(self, k,n,kgen,S,P,m):
+    def __init__(self, kgen):
         self.kgen = kgen
         self.k = 2**self.kgen - self.kgen -1
         self.n = 2**self.kgen -1
-        self.G = self.genhammingMatrix()
+        self.G = self.genHammingMatrix()
         self.S = self.genInvertibleMatrix()
         self.P = self.genPermuteMatrix()
-        self.Gcarat = np.matmul(np.matmul(self.S,self.G),self.P)
-        self.m = m
+        self.Gcarat = np.matmul(np.matmul(self.S, self.G), self.P) % 2
 
     def genHammingMatrix(self):
         """Generates Hamming Matrix given k and n"""
-        self.identity = numpy.identity(self.kgen)
-        self.identityk = numpy.identity(self.k)
-        self.left = np.zeroes(self.kgen,2**self.kgen - 1 - self.kgen)
-        self.rowcount = 0
+        identity = np.identity(self.kgen)
+        identityk = np.identity(self.k)
+        left = np.zeros((self.kgen, 2**self.kgen - 1 - self.kgen)).T
+        rowcount = 0
         for i in range(2**self.kgen):
-            if i+1 != 1:
-                if (1+1)%2 != 0:
-                    self.binarystring = np.binary_repr(i+1)
-                    self.column = np.zeros((length(self.binarystring),1))
-                    for i in binarystring:
-                        self.column[0][i] = int(i)
-                    self.column = np.pad(self.column, (1,self.kgen), 'constant')
-                    self.left[:][count] =  self.column
-        self.paritycheck = np.concatenate(self.left,self.identity)
-        self.generator = np.concatenate(self.identityk,np.transpose(self.left))
+            if i + 1 != 1:
+                if (i + 1) & i != 0:
+                    binarystring = np.binary_repr(i+1)
+                    column = np.zeros((len(binarystring), 1))
+                    for i in range(len(binarystring)):
+                        column[-i - 1] = binarystring[i]
+                    column = np.pad(column, (0, self.kgen - len(binarystring)), 'constant')
+                    left[rowcount] = column.T[0]
+                    rowcount += 1
+        left = left.T
+        self.paritycheck = np.block([left, identity])
+        self.generator = np.block([identityk, np.transpose(left)])
         return self.generator
 
     def genInvertibleMatrix(self):
-        self.S = np.random.randint(0,2,(self.k,self.k), dtype=np.uint)
+        S = np.random.randint(0,2,(self.k, self.k), dtype=np.uint)
         while np.linalg.det(S) == 0:
-            self.S = np.random.randint(0,2,(self.k,self.k), dtype=np.uint)
-        return self.S
-
+            S = np.random.randint(0,2,(self.k, self.k), dtype=np.uint)
+        return S
 
     def genPermuteMatrix(self):
-        self.P = np.identity(n, dtype=np.uint)
-        return self.P[np.random.permutation(self.n)]
+        P = np.identity(self.n, dtype=np.uint)
+        return P[np.random.permutation(self.n)]
+
+
+keys = hamming_keygen(3)
+print(keys.Gcarat)
